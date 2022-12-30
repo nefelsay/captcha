6b5708150a1d7938b41a5e15938336e434f8727a
nefelsay <65243638+nefelsay@users.noreply.github.com>
2022-12-30 18:15:29 +0300

    Update simple_captcha_2.py

--- simple_captcha_2.py
+++ simple_captcha_2.py
@@ -4,24 +4,26 @@ from twocaptcha import TwoCaptcha
 
 
 solver = TwoCaptcha('***********API code***********')
+#Создаём словарь для того чтобы положить в него результат овтета на API {'captchaId': '72447681441', 'code': 'gbkd'}
+dict_resut = {}
 img_name = 'img.png'
 def sender_solve(path=img_name):
     print('2) Изображение отправленно для разгадывания:')
     result = solver.normal(path)
     print('3) От API пришёл ответ: ', result)
-    #Функция вернёт словарь {'captchaId': '72447681441', 'code': 'gbkd'}
-    return result
+    #API вернёт словарь {'captchaId': '72447681441', 'code': 'gbkd'}
+    #Обновляем словарь для дальнейшего извлечения ID капчи и отправки репорта
+    dict_resut.update(result)
+    return result['code']
 
 
 with webdriver.Chrome() as browser:
     browser.get('https://captcha-parsinger.ru/?page=3')
     browser.implicitly_wait(10)
-
     if 'Подтвердите, что вы не робот' in browser.page_source:
         browser.find_element(By.CSS_SELECTOR, 'div[class="chakra-form-control css-1sx6owr"]').find_element(By.TAG_NAME,'img').screenshot(img_name)
         print('1) Скриншот области успешно сделан')
-        func_sender_solve = sender_solve(img_name)
-        browser.find_element(By.ID, 'field-:r0:').send_keys(func_sender_solve['code'])
+        browser.find_element(By.ID, 'field-:r0:').send_keys(sender_solve())
         browser.find_element(By.CSS_SELECTOR, 'button[class="chakra-button css-1wq39mj"]').click()
         #Запускаем бесконечный цикл на случай если неудачных попыток будет несколько.
         while True:
@@ -30,21 +32,23 @@ with webdriver.Chrome() as browser:
             #Если спарсить не получилось, и список name_card не содержит элементов то переходит в блок else
             if len(name_card) > 0:
                 #Репортим о успешном решении
-                solver.report(func_sender_solve['captchaId'], True)
-                print("Отправлен репорт о успешном разгадывании")
+
+                solver.report(dict_resut['captchaId'], True)
+                print(f"Отправлен репорт о успешном разгадывании. id:{dict_resut['captchaId']}")
                 print('4)',name_card)
                 #После успешного получения данных, прерываем цикл
                 break
             else:
                 print("Капча решена не верно, повторяем попытку")
                 #Репортим о неудачной попытки разагадать капчу
-                solver.report(func_sender_solve['captchaId'], False)
+                print(f"Отправлен репорт о не успешной попытке. id:{dict_resut['captchaId']}")
+                solver.report(dict_resut['captchaId'], False)
                 #Делаем новый скриншот, т.к. после неуспешной попытке капча обновилась
                 print('Повторный скриншот области')
                 browser.find_element(By.CSS_SELECTOR, 'div[class="chakra-form-control css-1sx6owr"]').find_element(
                     By.TAG_NAME, 'img').screenshot('img.png')
                 #Отправляем скриншот повторно
-                browser.find_element(By.ID, 'field-:r0:').send_keys(func_sender_solve['code'])
+                browser.find_element(By.ID, 'field-:r0:').send_keys(sender_solve())
                 #Кликаем по нопку повторно
                 browser.find_element(By.CSS_SELECTOR, 'button[class="chakra-button css-1wq39mj"]').click()
                 #Пытаемся наполнить список name_card найденными элементами
