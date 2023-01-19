f6ecf196186074c379722b1167ea5ebd524f78c9
nefelsay <65243638+nefelsay@users.noreply.github.com>
2023-01-19 16:53:33 +0300

    Update click_iframe.py

--- click_iframe.py
+++ click_iframe.py
@@ -7,33 +7,39 @@ from seleniumwire import webdriver
 
 def main():
     with webdriver.Chrome() as browser:
+        # 1)
         browser.get('https://captcha-parsinger.ru/v2?page=3')
         browser.implicitly_wait(10)
-        frames = browser.find_elements(By.TAG_NAME, "iframe")
 
         #Ожидаем доступность iframe и кликаем по готовности
         #iframe с чек боксом
+        # 2)
         WebDriverWait(browser, 10).until(
             EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, f"iframe[title='reCAPTCHA']")))
 
-        #клик по чекбоксу капчи внутри iframe
+        #Клик по чекбоксу капчи внутри iframe
+        # 3)
         WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.ID, 'recaptcha-anchor'))).click()
 
         browser.implicitly_wait(10)
         #возвращаемся к основному контексту веб-страницы.
+        # 4)
         browser.switch_to.default_content()
 
         #Ожидаем доступность iframe и кликаем по готовности
         #iframe с набором картинок
+        # 5)
         WebDriverWait(browser, 10).until(
             EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, f"iframe[title='текущую проверку reCAPTCHA можно пройти в течение ещё двух минут']")))
 
         #Ожидаем кликабельность кнопки с аудио
+        # 6)
         WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.ID, 'recaptcha-audio-button'))).click()
 
         #Находит тег аудио по ID и излекаем содержимое атрибута src
+        # 7)
         src = browser.find_element(By.ID, "audio-source").get_attribute("src")
         print(f"[INFO] Audio src: {src}")
 
